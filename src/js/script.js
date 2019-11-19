d1603cb24faa4726f183cae17034e0ec30f13511
Viacheslav Cunshin <cunshin@ukr.net>
2019-11-19 03:17:14 +0200

    added js solution

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,20 @@
-function findUniq(arr) {
-    let uniqueNumber1 = arr[0], uniqueNumber2;
-    arr[0] === arr[1]  ? (uniqueNumber1 = arr[0]) : (
-        arr[0] === arr[2]  ? (uniqueNumber1 = arr[0]) : (uniqueNumber1 = arr[1])) ;
-    arr.forEach(element => uniqueNumber1 != element ? uniqueNumber2 = element : "" );
-    return uniqueNumber2;
-  }
+function decode(r) {
+    let decodeKey = "", arrString = [], decodeMap = new Map(), rezult  = '';
 
-console.log(findUniq([ 1, 1, 1, 2, 1, 1 ]));
-console.log(findUniq([ 0, 0, 0.55, 0, 0 ]));
+    r.split("").forEach (element => {
+        element.codePointAt(0) < 97 ? decodeKey += element : arrString.push(element);
+    })
+    for (let i = 0; i < 26; i++) decodeMap.set(
+        (i * decodeKey % 26)* decodeKey % 26,
+        String.fromCodePoint(((i * decodeKey) % 26) + 97));
+    
+    if(decodeMap.size != 26) return 'Impossible to decode';
+
+    arrString.forEach(element => {
+        rezult += decodeMap.get(element.codePointAt(0) - 97);
+    });
+    return rezult;
+
+}
+console.log(decode("6015ekx"));
+console.log(decode("5057aan"));
