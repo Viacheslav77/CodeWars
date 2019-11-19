1b35280ab9ce1b9d2ab050dbe79da9e8c6408213
Viacheslav Cunshin <cunshin@ukr.net>
2019-11-19 03:06:09 +0200

    added javascript solution

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,11 @@
-function solution(str) {
-
-    let lenghtOfStr = str.length;
-    let isItaPair = lenghtOfStr - Math.trunc(lenghtOfStr / 2) * 2;
-
-    if (!(isItaPair === 0)) str += "_";
-
-    let splitStr = [];
-    for (let i = 0; i < lenghtOfStr; i += 2)  splitStr.push(str[i] + str[i + 1])
-
-    return splitStr;
+function array_diff(a, b) {
+    let result, c = a;
+    (a.length === 0 && b.length != 0) ? result = [] : ((b.length === 0 && a.length != 0) ? result = a : "");
+    if (result) return result;
+    a.length < b.length ? ( a = b, b = c ) : "";   
+    b.forEach(element => a = a.filter(item => item != element));
+    return a;
 }
 
-console.log(solution('abcdef'));
-console.log(solution('й'));
-console.log(solution(''));
+console.log(array_diff([1,2],[1]));
+console.log(array_diff([1,2,2,2,3],[2]));
