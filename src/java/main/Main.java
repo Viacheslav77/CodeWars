de86892311284a434097f8847458ebb5b892f184
Viacheslav Cunshin <cunshin@ukr.net>
2019-11-20 13:07:06 +0200

    delited ""

--- src/java/main/Main.java
+++ src/java/main/Main.java
@@ -10,8 +10,8 @@ class Suite2 {
 
     public static String game(long n) {
         long sizeOfChessboard = n * n;
-        return (sizeOfChessboard % 2) == 0 ? ("" + "[" + sizeOfChessboard / 2 + "]")
-                : ("" + "[" + sizeOfChessboard + ", 2]");
+        return (sizeOfChessboard % 2) == 0 ? ("[" + sizeOfChessboard / 2 + "]")
+                : ("[" + sizeOfChessboard + ", 2]");
 
     }
 }
